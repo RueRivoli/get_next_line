84773b844f241c0096306d3990c4513166794c0f
Florent GALLOIS <fgallois@e2r10p9.42.fr>
2017-02-21 16:32:37 +0100

    changement du header

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fgallois <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/06 15:15:33 by fgallois          #+#    #+#             */
-/*   Updated: 2017/02/14 18:26:56 by fgallois         ###   ########.fr       */
+/*   Updated: 2017/02/20 17:04:14 by fgallois         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,22 @@
 
 # include <fcntl.h>
 # include "libft/libft.h"
+# define EOL '\n'
+# include <sys/uio.h>
+# include <sys/types.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
-# define BUFF_SIZE	8
+# define BUFF_SIZE 1
 
-typedef struct s_line
+typedef struct		s_line
 {
-	int fd;
-	char *line;
-	struct s_line *next;
-}	t_line;
+	int				fd;
+	char			*line;
+	struct s_line	*next;
+}					t_line;
 
-int		get_next_line(int fd, char **line);
+int					get_next_line(int fd, char **line);
 
 #endif
-
-
